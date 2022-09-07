727142cffdedf1906bb13caff549093a12c8e32e
LeRoyce Pearson <contact@leroycepearson.dev>
2022-09-07 12:10:10 -0600

    Implement playing n0

--- index.js
+++ index.js
@@ -26,3 +26,40 @@ async function play_w9999() {
   oscillator.start(audio.currentTime);
   oscillator.stop(audio.currentTime + 1);
 }
+
+/// This is just playing a short white noise clip
+async function play_n0(frequency) {
+  const audio = await get_audio();
+
+  // Create an audio buffer to put noise in
+  const num_seconds = 0.5;
+  const num_samples = audio.sampleRate * num_seconds;
+  const num_samples_int = num_samples | 0;
+  const noise_buffer = audio.createBuffer(1, num_samples_int, audio.sampleRate);
+  
+  const noise_samples = noise_buffer .getChannelData(0);
+  for (let i = 0; i < num_samples_int; i += 1) {
+    noise_samples[i] = Math.random() * 2.0 - 1.0;
+  }
+  
+  // Create the node to play back the noise
+  const node = new AudioBufferSourceNode(audio, {
+    buffer: noise_buffer,
+    loop: true,
+  });
+
+  // Change the playback rate so we can get different "frequencies" of noise.
+  // We make middle C (440Hz) equal 1.0 playbackRate.
+  if (frequency) {
+    node.playbackRate = frequency / 440;
+  }
+  
+  node
+    .connect(new GainNode(audio, { gain: 0.3 }))
+    .connect(audio.destination);
+
+  node.start(audio.currentTime);
+  node.stop(audio.currentTime + 1);
+}
+
+
